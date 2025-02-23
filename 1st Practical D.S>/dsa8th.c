356487c04c59e746ae031974a278346403377268
Pratham Gediya <164309946+prathamgediya@users.noreply.github.com>
2025-02-23 20:46:42 +0530

    Update dsa8th.c

--- 1st Practical D.S>/dsa8th.c
+++ 1st Practical D.S>/dsa8th.c
@@ -1,47 +1,36 @@
 // implement a program to merge ( concatenation ) two string without using library function
 
-
-
 #include <stdio.h>
 
-int main() {
-    char str1[50], str2[50];  // Increased size to avoid overflow
-    int i = 0, j = 0;
-
-    printf("\n Enter First String: ");
+int main(){
+    char str1[25], str2[25];
+    int i=0, j=0;
+    
+    printf("\n Enter First String:");
     fgets(str1, sizeof(str1), stdin);
 
-    printf("\n Enter Second String: ");
+    printf("\n Enter Second String:");
     fgets(str2, sizeof(str2), stdin);
 
-    // Removing newline character from str1
-    while (str1[i] != '\0') {
-        if (str1[i] == '\n') {
-            str1[i] = '\0';
-            break;
-        }
+    // Find the end of str1
+    while (str1[i] != '\0' && str1[i] != '\n') {
         i++;
     }
-
-    // Removing newline character from str2
-    while (str2[j] != '\0') {
-        if (str2[j] == '\n') {
-            str2[j] = '\0';
-            break;
-        }
-        j++;
+    
+    // Remove newline character if present
+    if (str1[i] == '\n') {
+        str1[i] = '\0';
     }
 
     // Concatenation
-    int len = i;  // Store the length of str1
-    i = 0;
-    while (str2[i] != '\0') {
-        str1[len] = str2[i];
-        len++;
+    while (str2[j] != '\0' && str2[j] != '\n') {
+        str1[i] = str2[j];
         i++;
+        j++;
     }
-    str1[len] = '\0'; // Null terminate
 
-    printf("\n Concatenated String is: %s\n", str1);
+    str1[i] = '\0'; // Null terminate
+
+    printf("\n Concatenated string is %s", str1);
     return 0;
 }
