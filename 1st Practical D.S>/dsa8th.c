657f6511b1f2a8835c12ed68234bca273978e5a3
Pratham Gediya <164309946+prathamgediya@users.noreply.github.com>
2025-02-23 13:56:20 +0530

    Update dsa8th.c

--- 1st Practical D.S>/dsa8th.c
+++ 1st Practical D.S>/dsa8th.c
@@ -3,38 +3,44 @@
 
 #include <stdio.h>
 
-void concatenateStrings(char str1[], char str2[], char result[]) {
+int main() {
+    char str1[50], str2[50];  // Increased size to avoid overflow
     int i = 0, j = 0;
 
-    // Copy the first string into result
+    printf("\n Enter First String: ");
+    fgets(str1, sizeof(str1), stdin);
+
+    printf("\n Enter Second String: ");
+    fgets(str2, sizeof(str2), stdin);
+
+    // Removing newline character from str1
     while (str1[i] != '\0') {
-        result[i] = str1[i];
+        if (str1[i] == '\n') {
+            str1[i] = '\0';
+            break;
+        }
         i++;
     }
 
-    // Append the second string to result
+    // Removing newline character from str2
     while (str2[j] != '\0') {
-        result[i] = str2[j];
-        i++;
+        if (str2[j] == '\n') {
+            str2[j] = '\0';
+            break;
+        }
         j++;
     }
 
-    // Add null terminator to the result string
-    result[i] = '\0';
-}
+    // Concatenation
+    int len = i;  // Store the length of str1
+    i = 0;
+    while (str2[i] != '\0') {
+        str1[len] = str2[i];
+        len++;
+        i++;
+    }
+    str1[len] = '\0'; // Null terminate
 
-int main() {
-    char str1[100], str2[100], result[200];
-
-    printf("Enter the first string: ");
-    scanf("%s", str1);
-
-    printf("Enter the second string: ");
-    scanf("%s", str2);
-
-    concatenateStrings(str1, str2, result);
-
-    printf("The concatenated string is: %s\n", result);
-
+    printf("\n Concatenated String is: %s\n", str1);
     return 0;
 }
