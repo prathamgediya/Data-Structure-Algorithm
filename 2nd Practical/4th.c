d88fd473f80c151a39044f3d5fe9a90962f91be0
Pratham Gediya <164309946+prathamgediya@users.noreply.github.com>
2025-03-01 19:54:08 +0530

    Update 4th.c

--- 2nd Practical/4th.c
+++ 2nd Practical/4th.c
@@ -1,7 +1,4 @@
-// Today I don't have time to code !
-// 25/02 again i don't have much more time today.
 // Implement insert, delet algorithms of queue using array
-// This correct code !!!
 
 #include <stdio.h>
 #include <stdlib.h>
